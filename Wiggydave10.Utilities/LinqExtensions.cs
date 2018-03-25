e4fa1c4b8f8aa5bc4e64ee86f6cde860d510b88d
Matthew Yates <matthewy10@hotmail.com>
2018-03-25 19:37:10 +0100

    Added product equation class and helper

---
+++ Wiggydave10.Utilities/LinqExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Wiggydave10.Utilities
+{
+    public static class LinqExtensions
+    {
+        public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+        {
+            var seenKeys = new HashSet<TKey>();
+            foreach (var element in source)
+            {
+                if (seenKeys.Add(keySelector(element)))
+                    yield return element;
+            }
+        }
+    }
+}
---
+++ Wiggydave10.Utilities/Useful Classes/ProductEquation.cs
@@ -0,0 +1,83 @@
+﻿using System.Linq;
+
+namespace Wiggydave10.Utilities.Useful_Classes
+{
+    public class ProductEquation
+    {
+        private decimal multiplicand;
+        private decimal multiplier;
+        private decimal product;
+
+        public decimal Multiplicand
+        {
+            get
+            {
+                if (multiplicand > 0)
+                    return multiplicand;
+                return FindMultiplicand();
+            }
+            set => multiplicand = value;
+        }
+
+        public decimal Multiplier
+        {
+            get
+            {
+                if (multiplier > 0)
+                    return multiplier;
+                return FindMultiplier();
+            }
+            set => multiplier = value;
+        }
+
+        public decimal Product
+        {
+            get
+            {
+                if (product > 0)
+                    return product;
+                return FindProduct();
+            }
+            set => product = value;
+        }
+
+        public decimal FindProduct()
+        {
+            Product = Multiplicand * Multiplier;
+            return Product;
+        }
+
+        public decimal FindMultiplier()
+        {
+            Multiplier = decimal.Divide(Product, Multiplicand);
+            return Multiplier;
+        }
+
+        public decimal FindMultiplicand()
+        {
+            Multiplicand = decimal.Divide(Product, Multiplier);
+            return Multiplicand;
+        }
+
+        public string GetString()
+        {
+            return $"{Multiplicand}{Multiplier}{Product}";
+        }
+    }
+
+    public static class ProductEquationExtensions
+    {
+        public static bool IsPandigital(this ProductEquation productEq, int pandigital)
+        {
+            var str = productEq.GetString();
+            var numbers = Enumerable.Range(1, pandigital);
+
+            var distinctNumbers = str.ToCharArray().Distinct();
+            if (str.Length == pandigital && distinctNumbers.Count() == pandigital)
+            {
+                return numbers.All(number => str.Contains(number.ToString()));
+            }
+            return false;
+        }
+    }
+}
